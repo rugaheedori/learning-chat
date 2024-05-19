5594574bb07f186afa7b49c1c35d3b33f14113a6
hee <qufslarkgml@gmail.com>
2024-05-20 08:39:12 +0900

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module chat_server_golang
+
+go 1.22.3
+
+// js의 pacakge.json과 같은 역할
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "log"
+
+func init() {
+	log.Println("먼저 시작 됩니다.")
+}
+
+func main() {
+	log.Println("나중에 시작 됩니다.")
+}
