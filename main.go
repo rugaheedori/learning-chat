0a71d8ebf34c16b92b08269e7d145261f4088bd6
hee <qufslarkgml@gmail.com>
2024-05-20 09:04:48 +0900

    running a server with the gin framework

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "log"
+import (
+	"chat_server_golang/network"
+	"log"
+)
 
 func init() {
 	log.Println("먼저 시작 됩니다.")
@@ -8,4 +11,6 @@ func init() {
 
 func main() {
 	log.Println("나중에 시작 됩니다.")
+	n := network.NewServer()
+	n.StartServer()
 }
--- network/network.go
+++ network/network.go
@@ -1,1 +1,25 @@
 package network
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Network struct {
+	engin *gin.Engine
+}
+
+// 프레임워크를 사용할 수 있는 객체값 리턴함수 생성
+func NewServer() *Network {
+	n := &Network{engin: gin.New()}
+
+	return n
+}
+
+// 서버 시작 함수
+func (n *Network) StartServer() error {
+	log.Println("Starting Server")
+
+	return n.engin.Run(":8080")
+}
