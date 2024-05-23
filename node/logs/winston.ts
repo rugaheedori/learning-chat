5af879cf7de41e27eba30b555e9598ea517a7fec
hee <qufslarkgml@gmail.com>
2024-05-23 20:17:51 +0900

    set boilerplate logger

--- node/logs/winston.ts
+++ node/logs/winston.ts
@@ -1,4 +1,39 @@
-import { createLogger, format, transport } from "winston";
-import wintonDaily from "winston-daily-rotate-file";
+import { createLogger, format, transports } from "winston";
+import winstonDaily from "winston-daily-rotate-file";
 
 const logDri = "log";
+
+const logFormat = format.printf((info) => {
+  return `${info.timestmap} ${info.level} ${info.message}`;
+});
+
+const socketLogger = createLogger({
+  format: format.combine(
+    format.timestamp({
+      format: "YYYY-MM-DD HH:mm:ss",
+    }),
+    logFormat
+  ),
+  transports: [
+    new winstonDaily({
+      filename: "socket-info.log",
+      datePattern: "YYYY-MM-DD",
+      dirname: logDri + "/socket",
+      level: "info",
+    }),
+    new winstonDaily({
+      filename: "socket-err.log",
+      datePattern: "YYYY-MM-DD",
+      dirname: logDri + "/socket",
+      level: "error",
+    }),
+  ],
+});
+
+socketLogger.add(
+  new transports.Console({
+    format: format.combine(format.colorize(), format.simple()),
+  })
+);
+
+export {socketLogger}
