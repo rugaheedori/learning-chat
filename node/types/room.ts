aff1726e27cbc738227ee4100821ceb3754bfa54
hee <qufslarkgml@gmail.com>
2024-05-23 20:44:53 +0900

    set room type

---
+++ node/types/room.ts
@@ -0,0 +1,29 @@
+import WebSocket from "ws";
+import { socketLogger } from "../logs/winston";
+
+class Room {
+  private clients: Set<WebSocket>;
+
+  constructor() {
+    this.clients = new Set();
+  }
+
+  join(client: WebSocket) {
+    socketLogger.info("new client");
+    this.clients.add(client);
+  }
+
+  leave(client: WebSocket) {
+    socketLogger.info("removed client");
+    this.clients.delete(client);
+  }
+
+  forwardMessage(message: string) {
+    socketLogger.info("send message all clients");
+    for (const client of this.clients) {
+      client.send(JSON.stringify(message));
+    }
+  }
+}
+
+export { Room };
