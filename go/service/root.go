f51af7b07862e4814ac19d62924b2ffabd7c93b9
hee <qufslarkgml@gmail.com>
2024-05-28 00:18:09 +0900

    go: set service

--- go/service/root.go
+++ go/service/root.go
@@ -1,6 +1,10 @@
 package service
 
-import "chat_server_golang/repository"
+import (
+	"chat_server_golang/repository"
+	"chat_server_golang/types/schema"
+	"log"
+)
 
 type Service struct {
 	repository *repository.Repository
@@ -10,4 +14,40 @@ func NewService(repository *repository.Repository) *Service {
 	s := &Service{repository: repository}
 
 	return s
+}
+
+func (s *Service) GetChatList(roomName string) ([]*schema.Chat, error) {
+	if res, err := s.repository.GetChatList(roomName); err != nil {
+		log.Println("Failed To Get All Chat LIist", "err", err.Error())
+		return nil, err
+	} else {
+		return res, nil
+	}
+}
+
+func (s *Service) RoomList() ([]*schema.Room, error) {
+	if res, err := s.repository.RoomList(); err != nil {
+		log.Println("Failed To Get All Room LIist", "err", err.Error())
+		return nil, err
+	} else {
+		return res, nil
+	}
+}
+
+func (s *Service) MakeRoom(name string) error {
+	if err := s.repository.MakeRoom(name); err != nil {
+		log.Println("Failed To Make New Room", "err", err.Error())
+		return err
+	} else {
+		return nil
+	}
+}
+
+func (s *Service) Room(name string) (*schema.Room, error) {
+	if res, err := s.repository.Room(name); err != nil {
+		log.Println("Failed To Get Room", "err", err.Error())
+		return nil, err
+	} else {
+		return res, nil
+	}
 }
