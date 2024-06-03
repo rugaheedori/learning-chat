4bdb1e020486d181da48d1ac8f103af854a92f97
hee <qufslarkgml@gmail.com>
2024-06-03 23:48:52 +0900

    go: when the server start, insert serverinfo

--- go/network/network.go
+++ go/network/network.go
@@ -3,6 +3,7 @@ package network
 import (
 	"chat_server_golang/service"
 	"log"
+	"net"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -41,9 +42,39 @@ func NewServer(service *service.Service, port string) *Server {
 	return s
 }
 
+func (s *Server) setServerInfo() {
+	// IP를 가져오고, IP를 기반으로 MySQL serverInfo 테이블 변경
+	if addrs, err := net.InterfaceAddrs(); err != nil {
+		panic(err.Error())
+	} else {
+		var ip net.IP
+
+		for _, addr := range addrs {
+			// 타입 상속 확인
+			if ipnet, ok := addr.(*net.IPNet); ok {
+				if !ipnet.IP.IsLoopback() && ipnet.IP.To4() != nil {
+					ip = ipnet.IP
+					break
+				}
+			}
+		}
+
+		if ip == nil {
+			panic("no ip address found")
+		} else {
+			if err = s.service.ServerSet(ip.String()+s.port, true); err != nil {
+				panic(err)
+			} else {
+				s.ip = ip.String()
+			}
+		}
+	}
+}
+
 // 서버 시작 함수
 func (s *Server) StartServer() error {
-	log.Println("Starting Server")
+	s.setServerInfo()
 
+	log.Println("Starting Server")
 	return s.engine.Run(s.port)
 }
